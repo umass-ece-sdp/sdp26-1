f2ef8706e5b3a5c9756d1198d0b383022b963873
thocmurphy <thocmurphy@umass.edu>
2025-11-14 12:30:45 -0500

    Update filepathing

--- software/lib/falcon.py
+++ software/lib/falcon.py
@@ -72,7 +72,7 @@ class FALCON(Tello):
         from the parent directory of the repository.***
         '''
         # Call to bash script to connect WiFi
-        path_to_script = self.file_path.joinpath(r'../scripts/connection_client.sh').as_posix()
+        path_to_script = self.file_path.parent.joinpath(r'/scripts/connection_client.sh').as_posix()
         command =  f'source {path_to_script}'
         cmd = [command, interface, ssid, password]
 
