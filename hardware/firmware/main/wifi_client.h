3c4a895ded9368de138a95c01f0cda8dd47e84f5
thocmurphy <thocmurphy@umass.edu>
2025-11-19 21:55:02 -0500

    Fixed ip address

--- hardware/firmware/main/wifi_client.h
+++ hardware/firmware/main/wifi_client.h
@@ -7,7 +7,7 @@
 // WiFi configuration
 #define WIFI_SSID "jetson_nano_wifi"
 #define WIFI_PASS "team1-falcon"
-#define HOST_IP "192.168.10.1"
+#define HOST_IP "192.168.20.1"
 #define PORT 5000
 #define WAIT_TIME 1000 // in milliseconds
 
