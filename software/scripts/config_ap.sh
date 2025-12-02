2529a41d2fa9ba93140c1070f34434f9adb64668
thocmurphy <thocmurphy@umass.edu>
2025-12-01 23:42:36 -0500

    Use sudo in script

--- software/scripts/config_ap.sh
+++ software/scripts/config_ap.sh
@@ -3,11 +3,11 @@
 set -e  # exit if any command fails
 
 # Make sure the correct ip address is assigned to the interface
-ip addr del 192.168.10.1/24 dev wlx200cc83f101b
-ip addr add 192.168.20.1/24 dev wlx200cc83f101b
-systemctl restart dnsmasq
+sudo ip addr del 192.168.10.1/24 dev wlx200cc83f101b 2>/dev/null || true
+sudo ip addr add 192.168.20.1/24 dev wlx200cc83f101b 2>/dev/null || true
+sudo systemctl restart dnsmasq
 
 # Ensure WiFi services are started and enabled
 echo "Starting WiFi services at $(date)" >&2
-systemctl start dnsmasq
-systemctl start hostapd
+sudo systemctl start dnsmasq
+sudo systemctl start hostapd
