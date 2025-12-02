73493a3fe084504371aa4b21611bc0a2f4c611fa
thocmurphy <thocmurphy@umass.edu>
2025-12-01 22:55:59 -0500

    Added configuring AP's IP addr to main

--- software/src/main.py
+++ software/src/main.py
@@ -2,6 +2,8 @@ from hardware.firmware import server
 import threading, socket
 from software.lib import falcon_vision as fv
 from software.lib import variables
+import subprocess
+from pathlib import Path
 
 def server_thread(conn: socket.socket, sock: socket.socket):
     '''Thread function to run the server'''
@@ -20,6 +22,10 @@ def drone_thread():
 
 def main():
     '''Main entry point for the application'''
+
+    # Configure the AP interface to be the correct IP address
+    subprocess.run(['bash', Path(__file__).parent.parent.joinpath('software', 'scripts', 'config_ap.sh')], check=True)
+
     # Start the server and connect to glove before starting threads
     conn, sock = server.server_init()
     
