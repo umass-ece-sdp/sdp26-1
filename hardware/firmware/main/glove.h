36eabeaabdfb411fd70859d120dfbcb17cfb4a85
thocmurphy <thocmurphy@umass.edu>
2026-04-02 11:03:40 -0400

    Add function for getting distance readings from UWB

--- hardware/firmware/main/glove.h
+++ hardware/firmware/main/glove.h
@@ -28,10 +28,13 @@ struct Packet
 #define FINGER_PIN_4 17
 #define IMU_SDA_PIN 38
 #define IMU_SCL_PIN 37
+#define UWB_RX_PIN 44 // UART RX — UWB Module
+#define UWB_TX_PIN 43 // UART TX — UWB Module
 
 // Function prototypes
 void setup_glove();
 void setup_IMU(Adafruit_MPU6050 &mpu, bool &mpuOK, const int &accelRange, const int &gyroRange, const int &filterBandwidth);
 void read_fingers(float (&reading)[4]);
 void read_IMU(Adafruit_MPU6050 &mpu, sensors_event_t &accel, sensors_event_t &gyro, float (&accel_reading)[3], float (&gyro_reading)[3]);
+float get_UWB_distance(HardwareSerial &uwbSerial, const char* tagAddress);
 void store_data(Packet &packet, const float (&finger_readings)[4], const float (&accel_readings)[3], const float (&gyro_readings)[3], const float &UWB_distance);
